d1141156c138f93e9a002251f761d8603f16d58a
kjone8371 <kjone8371@gmail.com>
2024-07-22 13:56:23 +0900

    주석

---
+++ test-spring/src/main/java/com/kjone/testspring/security/service/JpaUserDetailsService.java
@@ -0,0 +1,27 @@
+package com.kjone.testspring.security.service;
+
+
+import com.kjone.testspring.domain.user.Member;
+import com.kjone.testspring.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class JpaUserDetailsService implements UserDetailsService {
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        Member member = memberRepository.findByAccount(username).orElseThrow(
+                () -> new UsernameNotFoundException("Invalid authentication!")
+        );
+
+        return new CustomUserDetails(member);
+    }
+}
