561cee81c93b0974d72194f8e1ef6e1381b8e629
kjone8371 <kjone8371@gmail.com>
2024-07-22 13:56:55 +0900

    주석

---
+++ test-spring/src/main/java/com/kjone/testspring/domain/user/SignResponse.java
@@ -0,0 +1,37 @@
+package com.kjone.testspring.domain.user;
+
+import com.kjone.testspring.domain.role.Authority;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class SignResponse {
+
+    private Long id;
+
+    private String account;
+
+    private String nickname;
+
+    private String name;
+
+    private String email;
+
+    private List<Authority> roles = new ArrayList<>();
+
+    private String token;
+
+    public SignResponse(Member member) {
+        this.id = member.getId();
+        this.account = member.getAccount();
+        this.nickname = member.getNickname();
+        this.name = member.getName();
+        this.email = member.getEmail();
+        this.roles = member.getRoles();
+    }
+}
